2e735a7e34deed99d20dee7901a0938feb30ce0b
ITHUB\Student <tsaturovds23@st.ithub.ru>
2025-03-17 16:32:47 +0300

    add State

--- Pacman/Assets/Source/Core/BootStrapper.cs
+++ Pacman/Assets/Source/Core/BootStrapper.cs
@@ -9,11 +9,20 @@ public class BootStrapper : MonoBehaviour
     //TODO: validation for interface implementation or use another field type
     [SerializeField] private InputListener inputListener;  
     private PlayerController _playerController;
+    public StateMachine StateMachine;
+    private int _index;
+    private AState[] _states;
 
     private void Awake()
     {
+        StateMachine = new StateMachine();
         Debug.Log("START!");
         _playerController = new (playerView, inputListener);
+        _states = new AState[]
+        {
+
+        };
+        StateMachine.ChangeState(_states[_index]);
     }
 
     private void Start()
@@ -24,5 +33,9 @@ public class BootStrapper : MonoBehaviour
     private void OnDisable()
     {
         _playerController.Expose();
+    }
+    private void Update()
+    {
+        StateMachine.Update();
     }
 }
